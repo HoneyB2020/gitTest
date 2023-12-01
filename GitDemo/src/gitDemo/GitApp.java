05878821044fb9e5290c79f0d96868cae09fdf4c
300371634 <300371634@N6107S28.ad.douglascollege.ca>
2023-12-01 11:12:39 -0800

    Roshan 1st commit

--- GitDemo/src/gitDemo/GitApp.java
+++ GitDemo/src/gitDemo/GitApp.java
@@ -5,6 +5,7 @@ public class GitApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Roshan");
+		System.out.println("Hey Karim");
 	}
 
 }
